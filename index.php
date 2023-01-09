164957e44ce7d60b27c90528e461541b3401e070
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-09 14:24:33 +0100

    Sostituzione text con asterischi

--- index.php
+++ index.php
@@ -7,12 +7,24 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- Stampa testo e lunghezza -->
     <h1>
         <?php
         $text = '    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit labore velit deleniti, exercitationem, fuga hic quae obcaecati eum consequuntur ipsa ex necessitatibus aliquam modi nihil facilis alias ratione est similique.';
         $length = strlen($text) ;
-        echo $text . ' , length = ' . $length;
+        $paragrafer= $text . ' Length = ' . $length . ' characters';
+        echo $paragrafer
         ?>
     </h1>
+
+
+    <!-- nasconde le parole dell'utente -->
+    <h2>
+        <?php
+            $hiddenWord = '';
+            $result= str_replace($_GET['hiddenWord'],"***", $paragrafer);
+            echo $result
+        ?>
+    </h2>
 </body>
 </html>
