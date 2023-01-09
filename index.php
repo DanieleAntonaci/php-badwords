ca14dbb8403e138ce93c7274e40bc986533897af
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-09 13:59:00 +0100

    crezione variabile stringa e lunghezza

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+        $text = '    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit labore velit deleniti, exercitationem, fuga hic quae obcaecati eum consequuntur ipsa ex necessitatibus aliquam modi nihil facilis alias ratione est similique.';
+        $length = strlen($text) ;
+        echo $text . ' , length = ' . $length;
+        ?>
+    </h1>
+</body>
+</html>
